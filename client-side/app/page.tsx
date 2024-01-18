d5e80188930d2b7e6fe90666d825012d334eaecc
Shivani Khatri <khatri.shivani96@gmail.com>
2024-01-19 00:43:10 +0100

    message has been restyled

--- client-side/app/page.tsx
+++ client-side/app/page.tsx
@@ -17,13 +17,16 @@ export default function Home() {
   return (
     <main className={styles.pageLayout}>
       <div className={styles.card}>
-        <img src="/assets/thankyou-image.jpg" alt="Thank you card"
-          className={styles.image}/>
+        <img
+          src="/assets/thankyou-image.jpg"
+          alt="Thank you card"
+          className={styles.image}
+        />
       </div>
-      <div>
+      <div className={styles.detail}>
         A huge Thank you to everyone who has helped me in my journey of becoming
-        a Frontend Developer. You have inspired me to learn, grow and be more
-        confident. I will forever be grateful for it.
+        a Frontend Developer.<br/> You have inspired me to learn, grow and be more
+        confident. <br/> I will forever be grateful for it.
       </div>
 
       {people.map((person: any) => (
