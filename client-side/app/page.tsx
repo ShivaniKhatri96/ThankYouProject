62c2a42192c2cac53c1edb65eb855c4a47385518
Shivani Khatri <khatri.shivani96@gmail.com>
2024-01-23 23:13:15 +0100

    code cleaned

--- client-side/app/page.tsx
+++ client-side/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
-import { useEffect, useState } from "react";
 import People from "./components/People";
 
 export default function Home() {
